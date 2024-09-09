ef55ecccc6e3f757c63f32383c37d90faf90ec6d
Michele Colella <michelecolella0@gmail.com>
2024-09-09 17:00:13 +0200

    Aggiornato la dimensione del portale

--- Assets/MainAssets/Script/NPCSystem/NavMeshGenerator.cs
+++ Assets/MainAssets/Script/NPCSystem/NavMeshGenerator.cs
@@ -6,24 +6,59 @@ using Unity.AI.Navigation;
 public class NavMeshGenerator : MonoBehaviour
 {
     [SerializeField] private float generationDelay = 5f;
+    [SerializeField] private float updateInterval = 2f; // intervallo tra gli aggiornamenti
     public NavMeshSurface surface;
-    
+
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
     void Start()
     {
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
         StartCoroutine(GenerateNavMeshDelayed());
+        StartCoroutine(UpdateNavMeshPeriodically());
     }
 
     IEnumerator GenerateNavMeshDelayed()
     {
         yield return new WaitForSeconds(generationDelay);
+        BuildNavMesh();
+    }
+
+    IEnumerator UpdateNavMeshPeriodically()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(updateInterval);
+            if (HasTransformChanged())
+            {
+                BuildNavMesh();
+            }
+        }
+    }
+
+    private void BuildNavMesh()
+    {
         if (surface != null)
         {
             surface.BuildNavMesh();
-            Debug.Log("NavMesh generato con successo dopo " + generationDelay + " secondi.");
+            Debug.Log("NavMesh aggiornato.");
         }
         else
         {
-            Debug.LogError("NavMeshSurface non trovato. Assicurati di aggiungere il componente NavMeshSurface all'oggetto.");
+            Debug.LogError("NavMeshSurface non trovato.");
         }
+    }
+
+    private bool HasTransformChanged()
+    {
+        if (transform.position != lastPosition || transform.rotation != lastRotation)
+        {
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+            return true;
+        }
+        return false;
     }
 }
